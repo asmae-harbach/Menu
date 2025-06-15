da9b7830bac7ef95113e725583befaf58309e803
asmae-harbach <asmaharbach@gmail.com>
2025-06-15 22:22:44 +0200

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const conn = async()=>{
     try {
-        const response = await mongoose.connect("mongodb+srv://asmaharbach:asmaa28@cluster0.wtec4rr.mongodb.net/duo");
+        const response = await mongoose.connect("mongodb+srv://username:password@cluster0.wtec4rr.mongodb.net/duo");
         if(response){
             console.log('connected to DB')
         }
