4a4cdb9d95fa9153efb7e0d68f8b5677dc4b978e
asmae-harbach <asmaharbach@gmail.com>
2026-01-02 20:12:34 +0100

    update

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -9,7 +9,7 @@ const Products = ({isOpen, setIsOpen, setData}) => {
         <div className='flex flex-wrap justify-center items-center gap-5 py-5'>
             {cate.map(item=>(
                 <div className='flex flex-col justify-around gap-4 w-[200px] h-[360px] bg-white border-2 border-transparent hover:border-color2 rounded-lg my-3'>
-                    <img className='w-full h-[150px]' src={`http://localhost:4000/uploads/${item.image}`} alt="" />
+                    <img className='w-full h-[150px]' src={`${process.env.REACT_APP_API_URL}/uploads/${item.image}`} alt="" />
                     <div className='flex flex-col justify-center items-center gap-2 px-4'>
                         <h4 className='text-color2 font-semibold text-center'>{item.name?.slice(0, 30) || ""}</h4>
                         <p className='text-center text-[12px] text-gray-600 font-semibold'>{item.description?.slice(0, 34) || ""}...</p>
