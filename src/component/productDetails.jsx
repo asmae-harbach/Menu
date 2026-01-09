d9c1b2ef4e5f80017789f967edfa3f4e900ff6e5
asmae-harbach <asmaharbach@gmail.com>
2026-01-09 16:03:31 +0100

    'update'

--- src/component/productDetails.jsx
+++ src/component/productDetails.jsx
@@ -4,7 +4,7 @@ const ProductDetails = ({isOpen, setIsOpen, data}) =>{
     return(
         <div className='bg-ma-couleur bg-opacity-70 w-[70%] md:w-[30%] h-[85%] md:h-[90%] rounded-xl flex flex-col gap-4 py-4'>
             <VscClose onClick={()=>setIsOpen(!isOpen)} className="m-2 cursor-pointer self-end text-[30px] text-white font-bold" />
-            <img className='w-full h-[50%] p-0' src={`${process.env.REACT_APP_API_URL}/uploads/${data.image}`} alt="" />
+            <img className='w-full h-[50%] p-0' src={item.image} alt="" />
             <div className='p-4 flex flex-col gap-4'>
                 <div className='flex justify-between text-white text-2xl font-semibold'>
                     <h1>{data.name}</h1>
